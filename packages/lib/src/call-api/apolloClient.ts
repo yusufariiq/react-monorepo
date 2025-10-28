ab4c3286c354474ec520f9b6cc863e830a10906d
ariiq <yusufariiq3@gmail.com>
2025-10-28 15:12:04 +0700

    feat(graphql): konfigurasi dengan graphql

---
+++ packages/lib/src/call-api/apolloClient.ts
@@ -0,0 +1,10 @@
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+
+const httpLink = new HttpLink({
+    uri: 'http://localhost:5000',
+})
+
+export const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+})
