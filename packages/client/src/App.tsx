871b58aa54dc2017a4cf63000da6b22322822f38
ariiq <yusufariiq3@gmail.com>
2025-10-28 16:06:34 +0700

    fix(page): modify main page to ensure there is no error after integrating with components and lib packages

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "components";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ padding: '2rem' }}>
+      <h1>Selamat Datang di Web Client</h1>
+
+      {/* Ini adalah komponen kustom Anda. 
+          Ini akan tampil sebagai tombol biru 'primary' */}
+      <Button onClick={() => alert('Clicked!')}>
+        Tombol dari components
+      </Button>
+
+      {/* Anda masih bisa menimpa default-nya */}
+      <Button type="dashed" style={{ marginLeft: 8 }}>
+        Tombol Dashed
+      </Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 import { ApolloProvider } from '@apollo/client/react';
-import { client } from 'lib'; 
+import { client } from 'lib';
+
+import 'antd/dist/reset.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
